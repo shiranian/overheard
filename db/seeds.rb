54d83d1197d30a39943c67471ca080263f673843
Alex Wheeler <afwheeler92@gmail.com>
2014-11-12 21:19:17 -0500

    deletes seed file since time for the real deal!!!!!!!

--- db/seeds.rb
+++
@@ -1,9 +0,0 @@
-   n = 0
-   5.times do
-   n += 1
-   podcast = Podcast.create(title: "example" + n.to_s, author: "author " + n.to_s, genre: "economics")
-   podcast = Podcast.create(title: "example" + n.to_s, author: "author " + n.to_s, genre: "surfing")
-   podcast = Podcast.create(title: "example" + n.to_s, author: "author " + n.to_s, genre: "psychology")
-   podcast = Podcast.create(title: "example" + n.to_s, author: "author " + n.to_s, genre: "titties")
-   podcast = Podcast.create(title: "example" + n.to_s, author: "author " + n.to_s, genre: "technology")
-   end
