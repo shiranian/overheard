bf0feed5222511b6c48bbba2e7cd85d1f3f0c8a4
Alex Wheeler <afwheeler92@gmail.com>
2015-01-27 15:44:12 -0500

    edits star modal to only ask star question

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -4,23 +4,12 @@ class RatingsController < ApplicationController
   end
 
   def create
-    puts "you hit me"
-    puts "yo"
-    puts "yo"
-    puts "yo"
-    puts "yo"
-    puts "yo"
-    puts "yo"
-    puts "yo"
-    puts "yo"
-    puts "yo"
-    puts params
     id =  params["podcastid"]
     relevance  = nil
      params["relevantrating"] == "yes" ? relevance = true : relevance = false
     stars = params["starvalue"]
     @podcast = Podcast.find(id)
-    @rating = @podcast.ratings.build({relevant: relevance, stars: stars.to_i})
+    @rating = @podcast.ratings.build({stars: stars.to_i})
     @rating.save
     redirect_to root_path
   end
