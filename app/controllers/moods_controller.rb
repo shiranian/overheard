97648343040a16617da45d88e52511a90b2f430e
Alex Wheeler <afwheeler92@gmail.com>
2015-02-10 12:54:14 -0500

    fixes rand() problem array length - 1 problem

--- app/controllers/moods_controller.rb
+++ app/controllers/moods_controller.rb
@@ -6,6 +6,6 @@ class MoodsController < ApplicationController
     session[:mood] = params[:mood]
     podcasts = Podcast.where("genre = ?",params[:mood])
     num_podcasts = podcasts.length
-    redirect_to podcast_path(podcasts[rand(1..num_podcasts)])
+    redirect_to podcast_path(podcasts[rand(num_podcasts)])
   end
 end
